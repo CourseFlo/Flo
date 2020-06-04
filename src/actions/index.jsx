4153a513bfa3d1542e2cb89dac5d085ea3a25e9c
Brandon <brandon.viro@gmail.com>
2020-06-04 04:30:15 -0700

    Create reducer, action, and component folders

---
+++ src/actions/index.jsx
@@ -0,0 +1,9 @@
+export const addMessage = (message) => {
+  return {
+    type: 'ADD_MESSAGE',
+    id: 0,
+    message
+  }
+}
+
+// TODO: ADD ACTIONS HERE
---
+++ src/reducers/index.tsx
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+// import someReducer from './reducerFile';
+
+export default combineReducers({
+//   reducer: someReducer              // TODO: ADD REDUCERS HERE
+});
