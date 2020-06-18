0f8423f5a6cc13c4d8708edea9b9ad9f6f1f8f4d
Guanting <16443280+baconandchips@users.noreply.github.com>
2020-06-17 17:36:37 -0700

    data structure change

--- src/redux/actions/visualCourse.tsx
+++ src/redux/actions/visualCourse.tsx
@@ -7,18 +7,17 @@ export const submitSearch = (filters: { query: string, letterCodes: Array<string
   };
 };
 
-export const displayCourse = (course: { courseCode: string, courseDescription: string }) => {
+export const updateCourse = (course: { courseCodeLetter: string, courseCodeNumber: number, courseLabel: string, courseDescription: string }) => {
   return {
     type: 'DISPLAY_COURSE',
-    courseCode: course.courseCode,
+    courseCodeLetter: course.courseCodeLetter,
+    courseCodeNumber: course.courseCodeNumber,
+    courseLabel: course.courseLabel,
     courseDescription: course.courseDescription,
   };
 };
 
-export const changeSelectedCourse = (course: { courseCode: string, courseDescription: string, prereqs: Array<string>, coreqs: Array<string>, future: Array<string>}) => {
-    return {
-        type: 'CHANGE_SELECTED_COURSE',
-        courseCode: course.courseCode, // SPLIT THE COURSECODE: LETTERS + NUMBERS; session number possible as well
+        // TODO: SPLIT THE COURSECODE: LETTERS + NUMBERS; session number possible as well
         // USE ID FOR THIS INSTEAD! Look up API documentation
         // Create data structure, something about courses, it'd have pseudo id; session 4 letter code and 3 digit number
         // Data exists in store, and nowhere else imo; map state to props! pull state into store through props. get from store to props
@@ -26,6 +25,11 @@ export const changeSelectedCourse = (course: { courseCode: string, courseDescrip
         // View can be the one that parses through the list
         // Reducer filters and puts a single course value
         // in reducer, we get ID, use it to apply filter to dictionary of courses, and save that group of course stuff as selected course
+
+export const changeSelectedCourse = (course: { courseCode: string, courseDescription: string, prereqs: Array<string>, coreqs: Array<string>, future: Array<string>}) => {
+    return {
+        type: 'CHANGE_SELECTED_COURSE',
+        courseCode: course.courseCode, 
         courseDescription: course.courseDescription,
         prereqs: course.prereqs,
         coreqs: course.coreqs,
