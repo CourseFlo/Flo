676682449b68abfd8665af7f12be2be89b5d0ffa
DSep <dyanatkar@me.com>
2020-07-15 20:23:49 -0700

    Fixing app

--- server/app.js
+++ server/app.js
@@ -77,4 +77,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
+});
+
 module.exports = app;
