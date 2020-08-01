bb8cecb22c0f0f635fbe612a285e126cb9eaabc3
Guanting <16443280+baconandchips@users.noreply.github.com>
2020-08-01 11:59:39 -0700

    login image

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import {Typography} from "@material-ui/core";
-import SearchCard from "../components/SearchCard";
+import { Typography } from '@material-ui/core';
+import SearchCard from '../components/SearchCard';
+import CourseReg from '../assets/course-reg.jpg';
 
 const Home = () => (
   <div>
     <Typography align="center" variant="h2">Welcome to CourseFlo</Typography>
     <SearchCard />
+    <img src={CourseReg} style={{ width: '100%', position: 'fixed', bottom: '0px' }} alt="text" />
   </div>
 );
 
