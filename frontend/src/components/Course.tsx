30eca18a011de2ca9da27ef330b49d2f0e218474
Brandon <34196752+beetai@users.noreply.github.com>
2020-08-01 23:53:00 -0700

    properly redirects visualize in browse

--- frontend/src/components/Course.tsx
+++ frontend/src/components/Course.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { useLocation, useHistory } from 'react-router-dom';
 
-import { Card, Typography, CardActions, CardContent, Button } from '@material-ui/core/';
+import {
+  Card,
+  Typography,
+  CardActions,
+  CardContent,
+  Button,
+  createStyles,
+  makeStyles,
+  Theme,
+} from '@material-ui/core/';
 
 import { openCourseModal } from '../redux/actions/modal';
 import FavButton from './FavButton';
 import { getVisualizedCourses } from '../redux/actions/courses';
-import {createStyles, makeStyles, Theme} from "@material-ui/core";
 
 const useStyles = makeStyles((theme: Theme) => createStyles({
   root: {
@@ -25,6 +34,14 @@ const useStyles = makeStyles((theme: Theme) => createStyles({
 function Course(props: any) {
   const { courseData, openCourseModal, getVisualizedCourses } = props;
   const classes = useStyles();
+  const location = useLocation();
+  const history = useHistory();
+
+  const handleVisualize = () => {
+    getVisualizedCourses(courseData.courseId);
+    if (location.pathname === '/Browse') history.push('/VisualCourse');
+  };
+  
 
   return (
     <Card className={classes.root}>
@@ -46,7 +63,7 @@ function Course(props: any) {
           Details
         </Button>
         <Button
-          onClick={() => getVisualizedCourses(courseData.courseId)}
+          onClick={handleVisualize}
           variant="outlined"
         >
           Visualize
