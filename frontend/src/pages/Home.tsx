7eb268db5e1c17a1943fe593c216e8c9fe996d00
Guanting <16443280+baconandchips@users.noreply.github.com>
2020-08-01 18:20:33 -0700

    in progress

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import { Typography } from '@material-ui/core';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
 import SearchCard from '../components/SearchCard';
 import CourseReg from '../assets/course-reg.jpg';
 
+/*
+const useStyles = makeStyles(() => createStyles({
+  splash: {
+    backgroundImage: url({courseReg}),
+  }
+})
+*/
+
 const Home = () => (
-  <div>
-    <div style={{ marginTop: '25%' }}>
+  <div style={{ background: `url(${CourseReg})`, height: '100vh', width: '100vw' }}>
+    <div style={{ /*marginTop: '25%'*/ }}>
       <Typography align="center" variant="h2">Welcome to CourseFlo</Typography>
       <SearchCard />
     </div>
-    <img src={CourseReg} style={{ width: '100%', position: 'fixed', bottom: '0px' }} alt="homepage" />
+    {/* <img src={CourseReg} style={{ width: '100%', position: 'fixed', bottom: '0px' }} alt="homepage" /> */}
   </div>
 );
 
